202901e61601dba43d57eec8c40d0af9ae695340
p0lluxstar <web-registra@yandex.ru>
2024-01-23 00:46:29 +0300

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+import fs from 'fs';
+
+const folderFiles = './src/fs/files/';
+const folderFilesCopy = './src/fs/files_copy/';
+
 const copy = async () => {
-    // Write your code here 
+
+    // Eсли files папка не существует или files_copy уже создана, Error
+    if (!fs.existsSync(folderFiles) || fs.existsSync(folderFilesCopy)) {
+        throw Error('FS operation failed');
+    }
+
+    // Создается папка files_copy
+    fs.mkdir(folderFilesCopy, err => {
+        
+        // Получение списка файлов и подпапок в исходной папке
+        const files = fs.readdirSync(folderFiles);
+
+        // Создаем файлы в папке сopy
+        files.forEach(file => {
+            fs.readFile(folderFiles + file, 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    fs.writeFile(`${folderFilesCopy + file} `, data, (err) => {
+                        if (err) console.log(err);
+                    })
+                }
+            })
+            return;
+        });
+
+        // не удалось создать папку
+        if (err) throw err;
+    });
+
 };
 
 await copy();
