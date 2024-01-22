186f63a348a69b46a70889681e514aeeac32b706
p0lluxstar <web-registra@yandex.ru>
2024-01-22 22:46:30 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+
+const pathFile = './src/fs/files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+
+    fs.access(pathFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            fs.writeFile(pathFile, 'I am fresh and young', (err) => {
+                if (err) console.log(err);
+            })
+            return;
+        }
+        throw Error('FS operation failed');
+    });
+
 };
 
 await create();
