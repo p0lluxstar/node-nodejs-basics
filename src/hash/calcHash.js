d92cc91f7f9d49b8e43e3d3c9fdfb21b69743449
p0lluxstar <web-registra@yandex.ru>
2024-01-23 22:25:58 +0300

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
+const file = './src/hash/files/fileToCalculateHashFor.txt'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(file);
+    const hash = crypto.createHash('sha256');
+
+    readStream.on('data', function (chunk) {
+        hash.update(chunk);
+    });
+
+    readStream.on('end', function () {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash);
+    });
+
+    readStream.on('error', function (error) {
+        console.error(error.message);
+    });
 };
 
 await calculateHash();
