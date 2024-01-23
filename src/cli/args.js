84ce35805bcc4a4b7eeaf2ef0dca59e3f0c5c174
p0lluxstar <web-registra@yandex.ru>
2024-01-23 17:02:26 +0300

    feat: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+const prefix = '--';
+let res = '';
+
 const parseArgs = () => {
-    // Write your code here 
+
+    process.argv.forEach((arg, index) => {
+        if (arg.startsWith(prefix)) {
+            res += `${arg.substring(2)} is ${process.argv[index + 1]}, `;
+        }
+    });
+
+    console.log(res.slice(0, -2));
 };
 
 parseArgs();
