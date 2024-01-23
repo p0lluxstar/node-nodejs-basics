e7058e0b8a3229bf2c2e51256187efdafae9e6d4
p0lluxstar <web-registra@yandex.ru>
2024-01-23 16:39:40 +0300

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+const prefix = 'RSS_';
+let res = '';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssVar = Object.keys(process.env).filter(key => key.startsWith(prefix));
+
+    rssVar.forEach(el => {
+        const value = process.env[el];
+        res += `${el}=${value}; `
+    });
+    
+    console.log(res.slice(0, -2));
 };
 
 parseEnv();
