d50ae57a3d99d31c5ca6fd4195e6935e64bec7cc
p0lluxstar <web-registra@yandex.ru>
2024-01-23 13:32:57 +0300

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+
+const oldFile = './src/fs/files/wrongFilename.txt';
+const newFile = './src/fs/files/properFilename.md'
+
 const rename = async () => {
-    // Write your code here 
+
+    // проверка существует ли файл
+    fs.access(oldFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            throw Error('FS operation failed');
+        } else {
+            // переименвоание файла
+            fs.rename(oldFile, newFile, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    });
+
+
 };
 
 await rename();
