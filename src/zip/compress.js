fe01ba96ca2e29b9eee22bb60cd3a2ac6a8cff25
p0lluxstar <web-registra@yandex.ru>
2024-01-26 00:09:12 +0300

    feat: compress.js and decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+const file = './src/zip/files/fileToCompress.txt';
+const gzipFile = './src/zip/files/archive.gz'
+
 const compress = async () => {
-    // Write your code here 
+    // Создаем поток для чтения исходного файла
+    const readStream = fs.createReadStream(file);
+
+    // Создаем поток для записи сжатого файла
+    const writeStream = fs.createWriteStream(gzipFile);
+
+    // Создаем поток для сжатия данных
+    const gzip = zlib.createGzip();
+
+    // Перенаправляем данные из потока чтения в поток сжатия
+    readStream.pipe(gzip)
+        // Перенаправляем данные из потока сжатия в поток записи
+        .pipe(writeStream)
+
+        // Обрабатываем завершение записи сжатого файла
+        .on('finish', () => {});
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+const gzipFile = './src/zip/files/archive.gz';
+const file = './src/zip/files/fileToCompress.txt';
+
 const decompress = async () => {
-    // Write your code here 
+    // Создаем поток для чтения архивного файла
+    const readStream = fs.createReadStream(gzipFile);
+
+    // Создаем поток для записи файла
+    const writeStream = fs.createWriteStream(file);
+
+    // Создаем поток для сжатия данных
+    const gzip = zlib.createGunzip();
+
+    // Перенаправляем данные из потока чтения в поток сжатия
+    readStream.pipe(gzip)
+        // Перенаправляем данные из потока сжатия в поток записи
+        .pipe(writeStream)
+
+        // Обрабатываем завершение записи сжатого файла
+        .on('finish', () => {});
 };
 
 await decompress();
